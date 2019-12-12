7c6289a5172270e3fb8626d9bd36ce68966ba0b3
JeremyBuisset <56023195+JeremyBuisset@users.noreply.github.com>
2019-12-12 09:51:08 +0100

    Media queries update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var cards=[1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12];
+var cards=[1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15,16,16];
 
-var cardsStatus=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var cardsStatus=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
 var cardsActive=[];
 
@@ -64,7 +64,7 @@ function game(nbCard){
                 display(cardsActive[0]);
                 display(cardsActive[1]);
                 cardsActive=[];
-                if(pairsFind === 12){
+                if(pairsFind === 16){
                     gameEnd();
                 }
             },750);
